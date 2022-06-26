ae7eebc43db3b64e7d75b34e46a1a966e606a26a
Ben Ashworth <bashworthj@gmail.com>
2022-06-26 16:18:46 -0400

    schema and seeds

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # TEAM-TRACKER
+
+
+## Table Of Contents
+1. [Description](#description)
+3. [Authors and Acknowledgement](#authors-and-acknowledgement)
+4. [Resources](#resources)
+
+## Description 
+-  
+
+
+## Authors and Acknowledgement
+- Ben Ashworth
+
+## Resources
+[Repo Link](https://github.com/bashworthj/TEAM-TRACKER) 
+
+[Video Link]()
+ 
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (name)
+VALUES
+('Sales'),
+('Tech'),
+('Accounting'),
+('Labor');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 100000, 1),
+('Sales Agent', 75000, 1),
+('Front End Developer', 115000, 2),
+('Back End Developer', 150000, 2),
+('Senior Accountant', 200000, 3),
+('Junior Accountant' 135000, 3),
+('Field Tech'. 85000, 4),
+('Driver', 65000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Ben', 'Ashworth', 1, null)
+('Sam', 'Becker', 2, 1)
+('Max', 'Mckinley', 3, null)
+('Cale', 'Tucker', 4, 3)
+('Brandon', 'Lackner', 5, null)
+('Justin', 'Winans', 6, 5)
+('Joey', 'Rueswald', 7, null)
+('Collin', 'Mondrick', 8, 7)
+
+
