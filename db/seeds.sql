73f75d9d340bc5d85555f4b1827c4ef2f4cb9785
Ben Ashworth <bashworthj@gmail.com>
2022-06-26 17:29:50 -0400

    added view functions, next is add

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,19 +12,19 @@ VALUES
 ('Front End Developer', 115000, 2),
 ('Back End Developer', 150000, 2),
 ('Senior Accountant', 200000, 3),
-('Junior Accountant' 135000, 3),
-('Field Tech'. 85000, 4),
+('Junior Accountant', 135000, 3),
+('Field Tech', 85000, 4),
 ('Driver', 65000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Ben', 'Ashworth', 1, null)
-('Sam', 'Becker', 2, 1)
-('Max', 'Mckinley', 3, null)
-('Cale', 'Tucker', 4, 3)
-('Brandon', 'Lackner', 5, null)
-('Justin', 'Winans', 6, 5)
-('Joey', 'Rueswald', 7, null)
-('Collin', 'Mondrick', 8, 7)
+('Ben', 'Ashworth', 1, null),
+('Sam', 'Becker', 2, 1),
+('Max', 'Mckinley', 3, null),
+('Cale', 'Tucker', 4, 3),
+('Brandon', 'Lackner', 5, null),
+('Justin', 'Winans', 6, 5),
+('Joey', 'Rueswald', 7, null),
+('Collin', 'Mondrick', 8, 7);
 
 
--- server.js
+++ server.js
@@ -4,10 +4,105 @@ const cTable = require('console.table');
 
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'employee_db'
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
     },
     console.log(`Connected to the employee_db database.`)
-  );
+);
+
+db.connect(function (err) {
+    if (err) throw err;
+    askUser();
+})
+
+const askUser = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'choice',
+        message: 'Welcome to Employee Database! What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    }).then(function (answers) {
+        switch (answers.choice) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmpRole();
+                break;
+            default:
+                break;
+
+        }
+    })
+};
+
+function viewDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+        if (err) throw err;
+        console.table('Viewing All Departments:', results);
+        askUser();
+    });
+};
+
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+        if (err) throw err;
+        console.table('Viewing All Roles:', results);
+        askUser();
+    });
+};
+
+function viewEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        if (err) throw err;
+        console.table('Viewing All Employees:', results);
+        askUser();
+    });
+};
+
+function addDepartment() {
+    db.query('SELECT * FROM department', function (err, results){
+        if (err) throw err;
+        inquirer.prompt({
+            type: 'input',
+            name: 'newDepo',
+            message: 'Input the department you would like too add.'
+        }).then(function (answer){
+            db.query('INSERT INTO department SET')
+        })
+    })
+    
+};
+
+function addRole() {
+
+};
+
+function addEmployee() {
+
+};
